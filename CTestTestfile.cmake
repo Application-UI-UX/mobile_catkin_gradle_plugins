e53faec826021665e4506991585d996cf337d1f8
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-12-06 16:55:00 -0500

    recal

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_bootstrap
-# Build directory: /home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_bootstrap
+# Source directory: /home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_bootstrap
+# Build directory: /home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_bootstrap
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
